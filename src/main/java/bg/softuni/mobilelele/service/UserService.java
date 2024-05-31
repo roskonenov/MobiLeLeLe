3e1935a0e74d55b4e503c19fd784d5e0b6674ee8
Rosen <roskonenov@gmail.com>
2024-05-31 19:09:18 +0300

    Add controller and service for user registration and login

---
+++ src/main/java/bg/softuni/mobilelele/service/UserService.java
@@ -0,0 +1,7 @@
+package bg.softuni.mobilelele.service;
+
+import bg.softuni.mobilelele.model.dto.UserRegisterDTO;
+
+public interface UserService {
+    void registerUser(UserRegisterDTO userRegisterDTO);
+}
---
+++ src/main/java/bg/softuni/mobilelele/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package bg.softuni.mobilelele.service.impl;
+
+import bg.softuni.mobilelele.model.dto.UserRegisterDTO;
+import bg.softuni.mobilelele.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void registerUser(UserRegisterDTO userRegisterDTO) {
+        //todo
+    }
+}
